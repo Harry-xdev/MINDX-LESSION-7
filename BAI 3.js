a1fa8046d7c33711c9e941eddf76a953f7a89539
Harry-xdev <96917441+Harry-xdev@users.noreply.github.com>
2022-01-06 09:05:18 +0700

    Update BAI 3.js

--- BAI 3.js
+++ BAI 3.js
@@ -1,7 +1,4 @@
-document.write("3/ F temperature to C")
 let f = prompt("Temperature in F:")
 const i = 32/1.8
 let c = (f / 1.8) - i
-document.write("in F:", f)
-document.write("in C:", c)
-document.write("--------")
+document.write(f,"F","=",c,"C")
