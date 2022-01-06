4cf4f879a6dcfa8f44d035ed6c4ddda6d37442da
Harry-xdev <96917441+Harry-xdev@users.noreply.github.com>
2022-01-06 08:08:42 +0700

    console to document

--- js/BAI 1 2 3 4.js
+++ js/BAI 1 2 3 4.js
@@ -1,29 +1,29 @@
-console.log("1/ Square root of a integer")
-let a = prompt("Square root of:")
-console.log("Square root of:",a)
-let b = Math.sqrt(a)
-console.log("Result:", b)
-console.log("--------")
+document.write("1/ Square root of a integer");
+let a = prompt("Square root of:");
+document.write("Square root of:",a);
+let b = Math.sqrt(a);
+document.write("Result:", b);
+document.write("--------")
 
-console.log("2/ Area of triangle")
-let h = prompt("Height of triangle:")
-let b2 = prompt("Bottom of triangle:")
-console.log("Height:", h, "CM")
-console.log("Bottom:", b2, "CM")
-let s = (h * b2) / 2
-console.log("Area of triangle:",s,"CM SQUARE")
-console.log("--------")
+document.write("2/ Area of triangle");
+let h = prompt("Height of triangle:");
+let b2 = prompt("Bottom of triangle:");
+document.write("Height:", h, "CM");
+document.write("Bottom:", b2, "CM");
+let s = (h * b2) / 2;
+document.write("Area of triangle:",s,"CM SQUARE");
+document.write("--------");
 
-console.log("3/ F temperature to C")
-let f = prompt("Temperature in F:")
-const i = 32/1.8
-let c = (f / 1.8) - i
-console.log("in F:", f)
-console.log("in C:", c)
-console.log("--------")
+document.write("3/ F temperature to C");
+let f = prompt("Temperature in F:");
+const i = 32/1.8;
+let c = (f / 1.8) - i;
+document.write("in F:", f);
+document.write("in C:", c);
+document.write("--------");
 
-console.log("4/ KILOMET to MET")
-let km = prompt("Kilomet:")
-const z = 1000
-let mt = km * z
-console.log(km,"Km =",mt,"M")
+document.write("4/ KILOMET to MET");
+let km = prompt("Kilomet:");
+const z = 1000;
+let mt = km * z;
+document.write(km,"Km =",mt,"M");
