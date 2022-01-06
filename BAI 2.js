18410057536d2e9f27195ba546cad806e954974a
Harry-xdev <96917441+Harry-xdev@users.noreply.github.com>
2022-01-06 09:03:26 +0700

    Update BAI 2.js

--- BAI 2.js
+++ BAI 2.js
@@ -1,8 +1,4 @@
-document.write("2/ Area of triangle")
 let h = prompt("Height of triangle:")
 let b2 = prompt("Bottom of triangle:")
-document.write("Height:", h, "CM")
-document.write("Bottom:", b2, "CM")
 let s = (h * b2) / 2
-document.write("Area of triangle:",s,"CM SQUARE")
-document.write("--------")
+document.write("2/ Area of triangle:",s,"CM SQUARE")
