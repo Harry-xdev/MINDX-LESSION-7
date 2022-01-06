66a95cd8ba7c7a3246b2259160e0dac38853e101
Harry-xdev <96917441+Harry-xdev@users.noreply.github.com>
2022-01-06 08:57:57 +0700

    Add files via upload

---
+++ BAI 2.js
@@ -0,0 +1,8 @@
+document.write("2/ Area of triangle")
+let h = prompt("Height of triangle:")
+let b2 = prompt("Bottom of triangle:")
+document.write("Height:", h, "CM")
+document.write("Bottom:", b2, "CM")
+let s = (h * b2) / 2
+document.write("Area of triangle:",s,"CM SQUARE")
+document.write("--------")
---
+++ BAI 4.js
@@ -0,0 +1,6 @@
+document.write("4/ KILOMET to MET")
+let km = prompt("Kilomet:")
+const z = 1000
+let mt = km * z
+document.write(km,"Km =",mt,"M")
+document.write("--------")
